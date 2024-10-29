91e8ca4b6aa20b0d3bb969ceec7b63c0d637193a
mikemorter <mikemorter@gmail.com>
2024-10-29 14:33:54 -0600

    udated JS challenge

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,108 +1,81 @@
 class PokerHand {
   constructor(cards) {
-      // Is hand submitted a string then split individual values
-      this.cardsArray = typeof cards === 'string' ? cards.split(" ") : null;
-      console.log(this.cardsArray);
+    // Is hand submitted a string then split individual values
+    this.cardsArray = typeof cards === "string" ? cards.split(" ") : null;
   }
 
-  // Get card values
-  
-  getCardValues(cards, prop){
-      let cardValues = cards.map((card)=>{
-        if( prop === 'ranks' ){
-          let value = card.length === 3 ? card.slice(0,2) : card[0];
-          return value;
-        }
-        if( prop === 'suits' ){
-          let value = card.length === 3 ? card.slice(2) : card[1];
-          return value;
-        }
+  getRank() {
+    // Not valid hand
+    if (this.cardsArray.length !== 5 || this.cardsArray === null) {
+      return "Not a Valid Hand";
+    }
+
+    let values = this.parseHand(this.cardsArray, "values");
+    let sortedValues = values.sort((a, b) => a - b);
+    let rankOccurances = this.cardOccurrence(values);
+    let sortedOccurances = rankOccurances.sort().join("");
+
+    // Poker hand rankings
+    if (values.sort().join("") === "10AJKQ" && this.isFlush())
+      return "Royal Flush";
+    if (this.isStraight(sortedValues) && this.isFlush())
+      return "Straight Flush";
+    if (this.isStraight(sortedValues)) return "Straight";
+    if (this.isFlush()) return "Flush";
+    if (sortedOccurances === "1112") return "One Pair";
+    if (sortedOccurances === "122") return "Two Pair";
+    if (sortedOccurances === "113") return "Three of a Kind";
+    if (sortedOccurances === "23") return "Full House";
+    if (sortedOccurances === "14") return "Four of a Kind";
+    return "High Card";
+  }
+
+  // Get occurrence of each card
+
+  cardOccurrence(values) {
+    values.sort();
+    let cardSuit = [];
+    let prev;
+
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] !== prev) {
+        cardSuit.push(1);
+      } else {
+        cardSuit[cardSuit.length - 1]++;
+      }
+      prev = values[i];
+    }
+    return cardSuit;
+  }
+
+  // Get card values and suits
+  parseHand(cards, prop) {
+    let cardValues = cards.map((card) => {
+      if (prop === "values") return card.slice(0, -1);
+      if (prop === "suits") return card.slice(-1);
     });
     return cardValues;
   }
 
-// Get occurnce of each card
+  isFlush() {
+    let suits = this.parseHand(this.cardsArray, "suits");
+    return new Set(suits).size === 1;
+  }
 
-  cardOccurrence(values){
-    let a = [], b = [], prev;
-    values.sort();
-    for( let i = 0; i < values.length; i++ ) {
-      if ( values[i] !== prev ) {
-        a.push(values[i]);
-        b.push(1);
-      } else {
-        b[b.length-1]++;
+  isStraight(sortedValues) {
+    let valuesString = sortedValues.join("");
+    if (
+      valuesString === "109JKQ" ||
+      valuesString === "10AJKQ" ||
+      valuesString === "1089JQ" ||
+      valuesString === "10789J"
+    )
+      return true;
+    else
+      for (let i = 1; i < sortedValues.length; i++) {
+        if (sortedValues[i] - sortedValues[i - 1] !== 1) return false;
       }
-        prev = values[i];
-    } 
-    return [a, b];
-  }
-
-// Implement way to find flush
-
-  flush(){
-    let suits = this.getCardValues(this.cardsArray, 'suits');
-    let filteredSuits = suits.filter(function(item, pos) {
-      return suits.indexOf(item) === pos;
-    })
-    
-    return filteredSuits.length === 1 ? true : false;
-  }
-
-// Implement way to find straight
-
-  straight(arr){
-    function isOneGreater(element, index, arr){
-      return index === arr.length - 1 || parseInt(element, 10) + 1  === parseInt(arr[index + 1], 10);
-    }
-    return arr.every(isOneGreater);
-  }
-
-// Ranks
-
-  getRank() {
-    // result if none of other ranks are met
-    let result = "High Card";
-
-    // Not valid hand
-    if( this.cardsArray.length !== 5 || this.cardsArray === null ){
-        return "Not a Valid Hand";
-    }
-    
-    let ranks = this.getCardValues(this.cardsArray, 'ranks');
-    let rankOccurances = this.cardOccurrence(ranks);
-
-    // Poker hand rankings
-    switch(rankOccurances[1].sort().join('')){
-        case '1112':
-              result = "One Pair";
-              break;
-        case '122':
-              result = "Two Pair";
-              break;
-        case "113":
-              result = "Three of a Kind";
-              break;
-        case "23":
-              result = "Full House";
-              break;
-        case "14":
-              result = "Four of a Kind";
-              break;
-    }
-    if( this.flush() ){
-      result = "Flush";    
-    } 
-    if ( this.straight(ranks.sort()) ){
-      result = "Straight";      
-    }
-    if ( this.straight(ranks.sort()) && this.flush() ){
-      result = "Straight Flush";      
-    }
-    if( ranks.sort().join('') === "10AJKQ"  && this.flush()){
-      result = "Royal Flush";
-    }
-    return result;
+    return true;
   }
 }
 
--- challenge-js/test/pokerHandTest.js
+++ challenge-js/test/pokerHandTest.js
@@ -1,39 +1,39 @@
-var assert = require('assert');
-var PokerHand = require('../pokerHand.js');
+var assert = require("assert");
+var PokerHand = require("../pokerHand.js");
 
 // test Royal Flush
 
-describe('Rank a Royal Flush', function() {
-  it('Return royal flush when hand given', function() {
-    var hand = new PokerHand('As Ks Qs Js 10s');
-    assert.equal(hand.getRank(), 'Royal Flush');
+describe("Rank a Royal Flush", function () {
+  it("Return royal flush when hand given", function () {
+    var hand = new PokerHand("As Ks Qs Js 10s");
+    assert.equal(hand.getRank(), "Royal Flush");
   });
 });
 
 // test Pair
 
-describe('Rank a Pair', function() {
-  it('Return one pair when hand given', function() {
-    var hand = new PokerHand('Ah As 10c 7d 6s');
-    assert.equal(hand.getRank(), 'One Pair');
+describe("Rank a Pair", function () {
+  it("Return one pair when hand given", function () {
+    var hand = new PokerHand("Ah As 10c 7d 6s");
+    assert.equal(hand.getRank(), "One Pair");
   });
 });
 
 // test Two Pair
 
-describe('Rank Two Pair', function() {
-  it('Return two pair when hand given', function() {
-    var hand = new PokerHand('Kh Kc 3s 3h 2d');
-    assert.equal(hand.getRank(), 'Two Pair');
+describe("Rank Two Pair", function () {
+  it("Return two pair when hand given", function () {
+    var hand = new PokerHand("Kh Kc 3s 3h 2d");
+    assert.equal(hand.getRank(), "Two Pair");
   });
 });
 
 // test Flush
 
-describe('Rank A Flush', function() {
-  var hand = new PokerHand('Kh Qh 6h 2h 9h');
-  it('Return flush when hand given', function() {
-    assert.equal(hand.getRank(), 'Flush');
+describe("Rank A Flush", function () {
+  var hand = new PokerHand("Kh Qh 6h 2h 9h");
+  it("Return flush when hand given", function () {
+    assert.equal(hand.getRank(), "Flush");
   });
 });
 
@@ -41,81 +41,99 @@ describe('Rank A Flush', function() {
 
 // test Three of a Kind
 
-describe('Rank Three of a Kind', function() {
-  it('Return three of a kind when hand given', function() {
-    var hand = new PokerHand('Ac Ah As 5c Qh');
-    assert.equal(hand.getRank(), 'Three of a Kind')
-  })
-})
+describe("Rank Three of a Kind", function () {
+  it("Return three of a kind when hand given", function () {
+    var hand = new PokerHand("Ac Ah As 5c Qh");
+    assert.equal(hand.getRank(), "Three of a Kind");
+  });
+});
 
 // test Four of a Kind
 
-describe('Rank Four of a Kind', function() {
-  it('Return four of a kind when hand given', function() {
-    var hand = new PokerHand('Ac Ah As AD Qh');
-    assert.equal(hand.getRank(), 'Four of a Kind')
-  })
-})
+describe("Rank Four of a Kind", function () {
+  it("Return four of a kind when hand given", function () {
+    var hand = new PokerHand("Ac Ah As AD Qh");
+    assert.equal(hand.getRank(), "Four of a Kind");
+  });
+});
 
 // test Full House
 
-describe('Rank Full House', function() {
-  it('Return full house when hand given', function() {
-    var hand = new PokerHand('Ac Ah As 5c 5d');
-    assert.equal(hand.getRank(), 'Full House')
-  })
-})
+describe("Rank Full House", function () {
+  it("Return full house when hand given", function () {
+    var hand = new PokerHand("Ac Ah As 5c 5d");
+    assert.equal(hand.getRank(), "Full House");
+  });
+});
 
 // test Straight
 
-describe('Rank Straight', function() {
-  it('Return a straight when hand given', function() {
-    var hand = new PokerHand('3c 4h 6d 5s 7h');
-    assert.equal(hand.getRank(), 'Straight')
-  })
-})
+describe("Rank Straight", function () {
+  it("Return a straight when hand given", function () {
+    var hand = new PokerHand("3c 4h 6d 5s 7h");
+    assert.equal(hand.getRank(), "Straight");
+  });
+});
+
+// test Straight with Face Cards
+
+describe("Rank Straight", function () {
+  it("Return a straight when hand that includes face cards given", function () {
+    var hand = new PokerHand("9c 10s 7d Jd 8h");
+    assert.equal(hand.getRank(), "Straight");
+  });
+});
+
+// test Straight with more Face Cards
+
+describe("Rank Straight", function () {
+  it("Return a straight when hand that includes more face cards given", function () {
+    var hand = new PokerHand("Ad 10s Jd Qc Kh");
+    assert.equal(hand.getRank(), "Straight");
+  });
+});
 
 // test Straight Flush
 
-describe('Rank Straight Flush', function() {
-  it('Return straight flush when hand given', function() {
-    var hand = new PokerHand('2s 3s 4s 6s 5s');
-    assert.equal(hand.getRank(), 'Straight Flush')
-  })
-})
+describe("Rank Straight Flush", function () {
+  it("Return straight flush when hand given", function () {
+    var hand = new PokerHand("2s 3s 4s 6s 5s");
+    assert.equal(hand.getRank(), "Straight Flush");
+  });
+});
 
 // test High Card
 
-describe('Rank High Card', function() {
-  it('Return high card when hand given', function() {
-    var hand = new PokerHand('Ac Qc 3d 5c 9s');
-    assert.equal(hand.getRank(), 'High Card')
-  })
-})
+describe("Rank High Card", function () {
+  it("Return high card when hand given", function () {
+    var hand = new PokerHand("Ac Qc 3d 5c 9s");
+    assert.equal(hand.getRank(), "High Card");
+  });
+});
 
 // test Short Hand
 
-describe('Short Input Hand', function() {
-  it('Return not a valid hand when short hand given', function() {
-    var hand = new PokerHand('2s Jd 4s 6s');
-    assert.equal(hand.getRank(), 'Not a Valid Hand')
-  })
-})
+describe("Short Input Hand", function () {
+  it("Return not a valid hand when short hand given", function () {
+    var hand = new PokerHand("2s Jd 4s 6s");
+    assert.equal(hand.getRank(), "Not a Valid Hand");
+  });
+});
 
 // test Long Hand
 
-describe('Long Input Hand', function() {
-  it('Return not a valid hand when long hand given', function() {
-    var hand = new PokerHand('2s Jd 4s 6s Qd Ks');
-    assert.equal(hand.getRank(), 'Not a Valid Hand')
-  })
-})
+describe("Long Input Hand", function () {
+  it("Return not a valid hand when long hand given", function () {
+    var hand = new PokerHand("2s Jd 4s 6s Qd Ks");
+    assert.equal(hand.getRank(), "Not a Valid Hand");
+  });
+});
 
 // test Incorrect Input
 
-describe('Handle incorrect input', function() {
-  it('Return not a valid hand when incorrect string given', function() {
-    var hand = new PokerHand('5h6h9s2h');
-    assert.equal(hand.getRank(), 'Not a Valid Hand');
+describe("Handle incorrect input", function () {
+  it("Return not a valid hand when incorrect string given", function () {
+    var hand = new PokerHand("5h6h9s2h");
+    assert.equal(hand.getRank(), "Not a Valid Hand");
   });
 });
